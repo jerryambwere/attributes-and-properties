e869b58405aaa904d2ba5eb95a071b8a8a7cb38b
Melvin jerry <+>
2024-10-26 11:01:57 +0300

    done

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+# /!/usr/bin/env python3
 
 APPROVED_BREEDS = [
     "Mastiff",
@@ -12,4 +12,47 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    APPROVED_BREEDS = ['Labrador', 'Beagle', 'Bulldog', 'Poodle', 'Rottweiler', 
+                       'German Shepherd', 'Golden Retriever', 'French Bulldog']
+
+    def __init__(self, name="Ashley", breed="Labrador") -> None:
+        self._name = name  # Private attribute for name
+        self._breed = breed  # Private attribute for breed
+
+    @property
+    def name(self):
+        print("Retrieving name!")
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            print(f"Setting name to {value}")
+            self._name = value
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    @property
+    def breed(self):
+        print("Retrieving breed!")
+        return self._breed
+
+    @breed.setter
+    def breed(self, value):
+        if value in self.APPROVED_BREEDS:
+            print(f"Setting breed to {value}")
+            self._breed = value
+        else:
+            print("Breed must be in list of approved breeds.")
+
+# Example Usage
+dog = Dog()
+print(dog.name)  # Retrieving name!
+dog.name = "Max"  # Setting name to Max
+print(dog.name)  # Retrieving name!
+dog.breed = "Bulldog"  # Setting breed to Bulldog
+print(dog.breed)  # Retrieving breed!
+dog.breed = "Unknown"  # Breed must be in list of approved breeds.
+
+
+
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,18 @@ APPROVED_JOBS = [
 ]
 
 class Person:
+    def __init__(self,name,job="Admin") -> None:
+        self.name=name
+        self.job=job
+
+    def get_job(self):
+        return self._job
+    
+    def set_job(self,value):
+        if value in Person.approved_jobs:
+            self._job=value
+        else:
+            print("Job must be in list of approved jobs.")
+
+        pass
     pass
